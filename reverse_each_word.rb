855bbe75bb801feafd35775b35c9dc1ca2fbddf5
Marques Avery <averymarques4@gmail.com>
2020-08-04 15:24:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ def reverse_each_word(sentence)
 #  reverse_list = []
   sentence_list = sentence.split
   sentence_list.collect do |word|
-    reverse_word = word.reverse
+#    reverse_word = word.reverse
 #    reverse_list << "#{reverse_word}"
+    word.reverse
   end
 #  reverse_list.join(" ")
 end
